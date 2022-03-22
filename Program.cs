466b2d577ce70d2a42e5d5ad819af8bb934021ee
Max Hansen <max.95hansen@gmail.com>
2022-03-22 13:53:28 +0100

    Moved CRUD commands into classes

--- Program.cs
+++ Program.cs
@@ -1,23 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Data.Sqlite;
+using Habit_Tracker;
 
-string connestionString = "Data Source=habitTracker.db";
+CreateDB createDB = new CreateDB();
+WriteAccesser writeAccesser = new WriteAccesser();
+ReadAccesser readAccesser = new ReadAccesser();
+UpdateAccesser updateAccesser = new UpdateAccesser();
+DeleteAccesser deleteAccesser = new DeleteAccesser();
 
-using (var connection = new SqliteConnection(connestionString))
-{
-    connection.Open();
-
-    var command = connection.CreateCommand();
-
-    command.CommandText =
-        @"CREATE TABLE IF NOT EXISTS breaks_table (
-            Id INTEGER PRIMARY KEY AUTOINCREMENT,
-            Date TIME,
-            Minutes INTEGER
-            )";
-    command.ExecuteNonQuery();
-}
-
+createDB.QueryInput();
 UserInput();
 
 void UserInput()
@@ -48,108 +38,26 @@ void UserInput()
                 closeApp = true;
                 break;
             case 1:
-                ExecuteRead();
+                readAccesser.ExecuteRead();
                 break;
             case 2:
-                ExecuteWrite();
+                writeAccesser.ExecuteWrite();
                 break;
             case 3:
-                ExecuteDelete();
+                deleteAccesser.ExecuteDelete();
                 break;
             case 4:
-                ExecuteUpdate();
+                updateAccesser.ExecuteUpdate();
                 break;
             default:
                 Console.WriteLine("\nPlease enter a valid number");
                 break;
         }
 
-        // Try user input for validity (not scaleable with switch/case current implementation)
-
     }
 }
 
-void ExecuteWrite()
-{
-    int minutes;
-
-    do
-    {
-        Console.WriteLine("How long was your leg-stretcher? (Time in minutes)");
-    } while (!int.TryParse(Console.ReadLine(), out minutes));
-
-    var connection = new SqliteConnection(connestionString);
-    connection.Open();
-
-    var command = connection.CreateCommand();
-
-    // Timestamp for when the entry was made and how long the break was
-    command.CommandText = $"INSERT INTO breaks_table(Date, Minutes) VALUES(DATETIME('NOW'), {minutes})";
-    command.ExecuteNonQuery();
-}
-
-void ExecuteRead()
-{
-    var connection = new SqliteConnection(connestionString);
-    connection.Open();
-
-    var command = connection.CreateCommand();
-
-    // Gets everything in the table
-    command.CommandText = "SELECT * FROM breaks_table";
-    SqliteDataReader reader = command.ExecuteReader();
-
-    // Not quite sure how data stream reading works but the code works
-    if(reader.HasRows)
-    {
-        while(reader.Read())
-        {
-            Console.WriteLine($"Entry number: {reader.GetString(0)}, Timestamp: {reader.GetString(1)}, Break time: {reader.GetInt32(2)} minutes");
-        }
-    }
-}
-
-void ExecuteUpdate()
-{
-    int id;
-    int minutes;
-
-    do
-    {
-        Console.WriteLine("Enter the entry number to change: ");
-    } while (!int.TryParse(Console.ReadLine(), out id));
-    do
-    {
-        Console.WriteLine("How long was your leg-stretcher? (Time in minutes)");
-    } while (!int.TryParse(Console.ReadLine(), out minutes));
-
-    var connection = new SqliteConnection(connestionString);
-    connection.Open();
-
-    var command = connection.CreateCommand();
-
-    // Could implement check for if the Id number exists in DB or not
-    // Timestamp for when the entry was made and how long the break was
-    command.CommandText = $"UPDATE breaks_table SET Minutes={minutes} WHERE Id={id}";
-    command.ExecuteNonQuery();
-}
-void ExecuteDelete()
-{
-    int id;
-
-    do
-    {
-        Console.WriteLine("Which entry do you want to delete?");
-    } while (!int.TryParse(Console.ReadLine(), out id));
-
-    var connection = new SqliteConnection(connestionString);
-    connection.Open();
-
-    var command = connection.CreateCommand();
-
-    command.CommandText = $"DELETE FROM breaks_table  WHERE Id = { id }";
-    command.ExecuteNonQuery();
-}
+// Try user input for validity (not scaleable with switch/case current implementation)
 static int TryInput(out int num)
 {
     string input = Console.ReadLine();
---
+++ SqliteDataAccess.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Habit_Tracker
+{
+    class DataAccesser
+    {
+        readonly string connestionString = "Data Source=habitTracker.db";
+
+        public virtual SqliteCommand QueryInput(string CommandString =
+            @"CREATE TABLE IF NOT EXISTS breaks_table (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Date TIME,
+                Minutes INTEGER
+                )")
+        {
+            var connection = new SqliteConnection(connestionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = CommandString;
+               
+            return command;
+        }
+    }
+    class CreateDB : DataAccesser
+    {
+        public CreateDB()
+        {
+            base.QueryInput();
+        }
+    }
+    class WriteAccesser : DataAccesser
+    {
+        private int minutes;
+        private string writeString;
+
+        public void ExecuteWrite()
+        {
+            do
+            {
+                Console.WriteLine("How long was your leg-stretcher? (Time in minutes)");
+            } while (!int.TryParse(Console.ReadLine(), out minutes));
+            writeString = $"INSERT INTO breaks_table(Date, Minutes) VALUES(DATETIME('NOW'), {minutes})";
+            QueryInput(writeString);
+            var commandHolder = QueryInput(writeString);
+            commandHolder.ExecuteNonQuery();
+        }
+    }
+
+    class ReadAccesser : DataAccesser
+    {
+        readonly string readString = "SELECT * FROM breaks_table";
+        public void ExecuteRead()
+        {
+            var commandHolder = QueryInput(readString);
+            SqliteDataReader reader = commandHolder.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Entry number: {reader.GetString(0)}, Timestamp: {reader.GetString(1)}, Break time: {reader.GetInt32(2)} minutes");
+                }
+            }
+        }
+    }
+
+    class DeleteAccesser : DataAccesser
+    {
+        string deleteString;
+        int id;
+
+        public void ExecuteDelete()
+        {
+            do
+            {
+                Console.WriteLine("Which entry do you want to delete?");
+            } while (!int.TryParse(Console.ReadLine(), out id));
+            deleteString = $"DELETE FROM breaks_table  WHERE Id = { id }";
+
+            var commandHolder = QueryInput(deleteString);
+            commandHolder.ExecuteNonQuery();
+        }
+    }
+    class UpdateAccesser : DataAccesser
+    {
+        int id;
+        int minutes;
+        string updateString;
+
+        public void ExecuteUpdate()
+        {
+            do
+            {
+                Console.WriteLine("Enter the entry number to change: ");
+            } while (!int.TryParse(Console.ReadLine(), out id));
+            do
+            {
+                Console.WriteLine("How long was your leg-stretcher? (Time in minutes)");
+            } while (!int.TryParse(Console.ReadLine(), out minutes));
+            updateString = $"UPDATE breaks_table SET Minutes={minutes} WHERE Id={id}";
+
+            QueryInput(updateString);
+            var commandHolder = QueryInput(updateString);
+            commandHolder.ExecuteNonQuery();
+        }
+    }
+}
